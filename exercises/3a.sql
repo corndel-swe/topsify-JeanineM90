81ea4b2d37787fdc539c2def559759867dc08516
Jeanine Marteau <jeanine.marteau@iris.co.uk>
2024-07-11 11:38:10 +0100

    ex2b

--- exercises/3a.sql
+++ exercises/3a.sql
@@ -1,1 +1,12 @@
 -- List the names of the 10 newest albums in the database
+
+/*
+select top 10 names * 
+from albums
+order by release_date desc
+*/
+
+select names  
+from albums
+order by release_date desc
+limit 10;
