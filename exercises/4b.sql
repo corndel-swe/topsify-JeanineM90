8cdf30213e36eb47cb83c3fe71a721f6fa986adb
Jeanine Marteau <jeanine.marteau@iris.co.uk>
2024-07-12 12:06:16 +0100

    4b

--- exercises/4b.sql
+++ exercises/4b.sql
@@ -5,7 +5,15 @@
 -- tempo is in features table 
 -- join on track id 
 
+/*
 select tra.id, tra.name, feat.tempo
+from tracks tra
+inner join features feat on feat.track_id = tra.id
+order by feat.tempo asc
+limit 10
+*/
+
+select feat.track_id, tra.name, feat.tempo
 from tracks tra
 inner join features feat on feat.track_id = tra.id
 order by feat.tempo asc
