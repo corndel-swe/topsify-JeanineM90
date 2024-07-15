728c7893665ed60cc3724966c695fc85315f8f66
Jeanine Marteau <jeanine.marteau@iris.co.uk>
2024-07-15 15:00:39 +0100

    update to 8b

--- exercises/8b.sql
+++ exercises/8b.sql
@@ -2,7 +2,15 @@
 -- Could you delete Dua Lipa's albums, too?
 -- Don't worry about the tracks, I'll get someone else to do that. kthx!
 
+/*
 DELETE FROM albums
 JOIN artists ON artists.name = albums.artist_id
 WHERE artists.name = 'Dua Lipa'
+*/
+
+-- use artist id fro artists.json:
+
+delete 
+from albums
+where artist.id = '6M2wZ9GZgrQXHCFfjv46we'
 
